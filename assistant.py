87a4e8957ea1f03cd825c417bbbda350f6712257
Andres Leon <andrewlion02@gmail.com>
2026-01-28 21:24:42 -0500

    mejoras del asistente en su logica

--- assistant.py
+++ assistant.py
@@ -1,4 +1,69 @@
 class BakeryAssistant:
+    def __init__ (self):
+        self.recipes_by_level = {
+            "Principiante": {
+                "focaccia": {
+                    "ingredientes": ["harina de trigo 1000g", "agua 350ml", "poolish 1200g (harina de trigo 600g + agua 600ml + azucar 2g + levadura 2g)", "sal 10g", "azucar 20g", "levadura 10g", "aceite de oliva 150ml"],
+                    "pasos": [ 
+                        "Paso 1: mezclar todos los ingredientes en un bol grande ecepto el poolish",
+                        "Paso 2: amasar por 15 minutos y formar una masa homogenea y lisa",
+                        "Paso 3: agregar el poolish y amasar nuevamente hasta optener una masa lisa",
+                        "Paso 4: dejar reposar la masa por 1 hora y media y dar pliegues cada 30 minutos ",
+                        "Paso 5: precalienta el horo 200 grados y hornear a 175 grados por 20 minutos",
+                    ]
+                }, 
+                "pan integral": {
+                    "ingredientes": ["harina integral 1000g", "agua 650ml", "levadura 10g", "sal 10g", "miel 120g", "mix de semillas de tu eleccion previamente hidratadas", "masa madre 200g"],
+                    "pasos": [
+                        "Paso 1: mezcla todos los ingredientes en un bol grande ecepto las semillas hasta obtener una masa homogenea y lisa",
+                        "Paso 2: agrega las semillas y mezcla",
+                        "Paso 3: deja reposar la masa por 1 hora",
+                        "Paso 4: hornea la masa por 20 minutos a 180 grados"
+                    ]
+                }
+            },
+            "Intermedio": {
+                "sourdough": {
+                    "ingredientes": ["harina 1000g", "agua 650ml", "levadura 3g", "sal 20g", "masa madre 200g"],
+                    "pasos": [
+                        "Paso 1: mezcla todos los ingredientes en un bol grande ecepto la masa madre",
+                        "Paso 2: agrega la masa madre y mezcla",
+                        "Paso 3: deja reposar la masa por 1 hora",
+                        "Paso 4: hornea la masa por 20 minutos a 250 grados"
+                    ]
+                }, 
+                "pan de zanahoria": {
+                    "ingredientes": ["harina 1000g", "agua 600ml", "levadura 20g", "sal 20g", "zanahoria 350g", "panela 80g", "mantequilla vegetal 50g","amapola 90g", "ralladura de 2 naranjas "],
+                    "pasos": [
+                        "Paso 1: mezcla todos los ingredientes en un bol grande ecepto la zanahoria",
+                        "Paso 2: agrega la zanahoria y mezcla",
+                        "Paso 3: deja reposar la masa por 1 hora",
+                        "Paso 4: hornea a 180 grados por 20 minutos"
+                    ]
+                }
+            },
+            "Avanzado": {
+             "pan de molde": {
+                    "ingredientes": ["harina 1000g", "leche 600ml", "levadura 20g", "sal 20g", "azucar 80g", "mantequilla 100g"],
+                    "pasos": [
+                        "Paso 1: mezcla todos los ingredientes en un bol grande ecepto la mantequilla",
+                        "Paso 2: agrega la mantequilla y mezcla",
+                        "Paso 3: deja reposar la masa",
+                        "Paso 4: hornea a 180 grados por 20 minutos"
+                    ]
+                }, 
+                "brioche": {
+                    "ingredientes": ["harina 1000g", "leche 350ml", "levadura 30g", "azucar 160g", "sal 10g", "huevos 5", "mantequilla 300g"],
+                    "pasos": [
+                        "Paso 1: mezcla todos los ingredientes en un bol grande ecepto la mantequilla",
+                        "Paso 2: agrega la mantequilla previamenete cortada en cubo y fria, mezclar hasta obtener una masa homogenea y lisa",
+                        "Paso 3: deja reposar la masa por 24 horas",
+                        "Paso 4: hornea a 170 grados por 20 minutos"
+                    ]
+                }
+            }
+        }
+
     def welcome(self, name):
         return f"Hola {name}, Mi nombre es Eurísaco, y seré tu asistente virtual en panadería. "
 
@@ -11,21 +76,17 @@ class BakeryAssistant:
             return "Avanzado"
 
     def show_recipes(self):
-        products = ["Focaccia", "Pan integral", "Sourdough", "Cinnamon roll", "Pan de molde", "Brioche"]
-        result = ""
-        for index, product in enumerate(products, start=1):
-            result += f"producto {index}: {product}\n"
+        result = "Recetas disponibles:\n"
+        for level, recipes in self.recipes_by_level.items():
+            result += f"nivel {level}:\n"
+            for recipe_name in recipes:
+                result += f"- {recipe_name}\n"
         
         return result
         
     def recommend_recipes(self, level):
-        recipes_by_level = {
-            "Principiante": ["focaccia", "pan integral"],
-            "Intermedio": ["sourdough", "cinnamon roll"],
-            "Avanzado": ["pan de molde", "brioche"]
-        }
-
-        recipes = recipes_by_level.get(level)
+        recipes = self.recipes_by_level.get(level)
+            
         if recipes is None:
             return "Lo siento, no hay recetas para tu nivel actual."
 
@@ -35,8 +96,34 @@ class BakeryAssistant:
         
         return result
         
+    def get_recipes_for_level(self, level):
+        return self.recipes_by_level.get(level, {})
 
+    def get_recipe_ingredients(self, level, recipe_name):
+        recipe = self.recipes_by_level.get(level, {}).get(recipe_name.lower())
+        if not recipe:
+            return "Lo siento receta no encontrada"
         
+        result = "ingredientes:\n"
+        for ingredient in recipe["ingredientes"]:
+            result += f"- {ingredient}\n"
+        
+        return result   
+
+    def get_steps(self, level, recipe_name):
+        recipe = self.recipes_by_level.get(level, {}).get(recipe_name.lower())
+        if not recipe:
+            return "Lo siento receta no encontrada"
+        
+        result = "paso a paso:\n"
+        for step in recipe["pasos"]:
+            result += f"- {step}\n"
+        
+        return result
+        
+  
+
+
 
     
 
--- main.py
+++ main.py
@@ -1,5 +1,39 @@
 from assistant import BakeryAssistant
 
+def show_recommended_recipes_menu(assistant, level):
+    recipes = assistant.get_recipes_for_level(level)
+    
+    if not recipes:
+        print("Lo siento, no hay recetas para tu nivel actual.")
+        return
+    
+    print("\nRecetas recomendadas para tu nivel:")
+    for recipe_name in recipes:
+        print(f"- {recipe_name}")
+
+    recipe_name = input("Que receta te gustaria ver?: ").strip().lower()
+
+    if recipe_name not in recipes:
+        print("Receta no encontrada.")
+        return
+    
+    while True:
+        submenu = input(
+            "\n1: Ver ingredientes\n "
+            "2: Ver paso a paso\n "
+            "3: Volver al menu principal\n "
+            "Por favor elige una option para continuar (1, 2, 3): ").strip().lower()
+
+        if submenu == "1":
+            print(assistant.get_recipe_ingredients(level, recipe_name))
+        elif submenu == "2":
+            print(assistant.get_steps(level, recipe_name))
+        elif submenu == "3":
+            print("Volviendo al menu principal")
+            break
+        else:
+            print("Por favor elige una opcion valida")
+
 assistant = BakeryAssistant()
 
 name = input("Cual es tu nombre?: ").strip().lower()
@@ -16,15 +50,21 @@ while True:
         "1: Mostrar recetas\n "
         "2: Rercomendar recetas\n "
         "3: Salir\n "
-        "Por favor elige una option valida (1, 2, 3): ").strip().lower()
+        "Por favor elige una option para continuar (1, 2, 3): ").strip().lower()
 
     if option == "1":
         print(assistant.show_recipes())
     elif option == "2":
-        print(assistant.recommend_recipes(level))
+        show_recommended_recipes_menu(assistant, level)
     elif option == "3":
         print("Saliendo del horno")
         break
     else:
-        print("Por favor elige una option valida") 
+        print("Por favor elige una opcion valida") 
+
+
+
+
+
+
 
